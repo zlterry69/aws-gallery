72daa4a7817db7a6930cbf6ae67f9a98776e2083
zlterry69 <terry.al@hotmail.com>
2023-09-03 22:00:25 -0500

    Backend and Frontend added

---
+++ backend/server.py
@@ -0,0 +1,82 @@
+import boto3
+import psycopg2
+from decouple import config
+
+
+from typing import List
+from pydantic import BaseModel
+
+import uvicorn
+from fastapi import FastAPI, UploadFile
+
+from fastapi.middleware.cors import CORSMiddleware
+
+S3_BUCKET_NAME = "YOUR_BUCKET_NAME"
+
+
+class VideoModel(BaseModel):
+    id: int
+    video_title: str
+    video_url: str
+
+
+app = FastAPI(debug=True)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/status")
+async def check_status():
+    return "Hello World!"
+
+
+@app.get("/videos", response_model=List[VideoModel])
+async def get_videos():
+    # Connect to our database
+    conn = psycopg2.connect(
+        database="exampledb", user="docker", password="docker", host="localhost",port="6543"
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM video ORDER BY id DESC")
+    rows = cur.fetchall()
+
+    formatted_videos = []
+    for row in rows:
+        formatted_videos.append(
+            VideoModel(id=row[0], video_title=row[1], video_url=row[2])
+        )
+
+    cur.close()
+    conn.close()
+    return formatted_videos
+
+
+@app.post("/videos", status_code=201)
+async def add_photo(file: UploadFile):
+    # Upload file to AWS S3
+    s3 = boto3.resource("s3")
+    bucket = s3.Bucket(S3_BUCKET_NAME)
+    bucket.upload_fileobj(file.file, file.filename)
+
+    uploaded_file_url = f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/{file.filename}"
+
+    # Store URL in database
+    conn = psycopg2.connect(
+       database="exampledb", user="docker", password="docker", host="localhost",port="6543"
+    )
+    cur = conn.cursor()
+    cur.execute(
+        f"INSERT INTO video (video_title, video_url) VALUES ('{file.filename}', '{uploaded_file_url}' )"
+    )
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=False)
